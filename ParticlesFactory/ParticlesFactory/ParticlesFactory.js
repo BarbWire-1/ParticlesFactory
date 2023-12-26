b8c860e030030448581dd412119bc89cc16bdd5a
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2023-12-26 13:30:08 +0100

    fixed download of config.json

--- ParticlesFactory/ParticlesFactory/ParticlesFactory.js
+++ ParticlesFactory/ParticlesFactory/ParticlesFactory.js
@@ -374,7 +374,7 @@ export class ParticlesFactory {
 	}
 
 	// ONLY IN EXAMPLE TO GET CURRENT STATE FOR CONFIG
-	async saveStatus2File() {
+	async savePropsStatus2File() {
 		// Get all properties (excluding methods and canvasEl) of the current instance
 		const properties = Object.keys(this).filter(
 			(key) =>
